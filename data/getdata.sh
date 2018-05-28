97f2cf0e6eb485ea292936b8619058c91f977cf6
KamilaLis <kamila.lis@outlook.com>
2018-05-28 11:01:17 +0000

    add:test data

--- data/getdata.sh
+++ data/getdata.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-# SGB Queen Graph -- do testów
 wget http://mat.gsia.cmu.edu/COLOR/instances/queen5_5.col
+wget http://mat.gsia.cmu.edu/COLOR/instances/inithx.i.1.col
+wget http://mat.gsia.cmu.edu/COLOR/instances/le450_15b.col
+wget http://mat.gsia.cmu.edu/COLOR/instances/queen13_13.col
